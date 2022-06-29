5911483642820bd3c614e292492ea95141dae11a
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2022-06-29 20:33:48 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function countAnimals(animal) {
-  // seu código aqui
+  const animals = species.reduce((acc, curr) => {
+    acc[curr.name] = curr.residents.length;
+    return acc;
+  }, {});
+  if (animal === undefined) {
+    return animals;
+  }
+  const { specie, sex } = animal;
+  const resultado = species.find((element) => element.name === specie);
+  if (sex !== undefined) {
+    return resultado.residents.filter((element) => element.sex === sex).length;
+  } return resultado.residents.length;
 }
 
 module.exports = countAnimals;

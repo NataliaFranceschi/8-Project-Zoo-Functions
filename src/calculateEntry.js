4706654f5836b1083e1261fb74a315d2b4993299
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2022-06-30 12:37:42 -0300

    Requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,10 +1,34 @@
 const data = require('../data/zoo_data');
 
-function countEntrants(entrants) {
+const { prices } = data;
+
+function countEntrants(entrants = 0) {
+  if (Object.entries(entrants).length === 0) {
+    return 0;
+  }
+  const child = [];
+  const adult = [];
+  const senior = [];
+  entrants.forEach((element) => {
+    if (element.age < 18) {
+      child.push(element);
+    } else if (element.age >= 50) {
+      senior.push(element);
+    } else {
+      adult.push(element);
+    }
+  });
+  return { adult: adult.length, child: child.length, senior: senior.length };
 }
 
-function calculateEntry(entrants) {
-  // seu código aqui
+function calculateEntry(entrants = 0) {
+  if (Object.entries(entrants).length === 0) {
+    return 0;
+  }
+  const count = countEntrants(entrants);
+  const { adult, child, senior } = count;
+  const total = adult * prices.adult + child * prices.child + senior * prices.senior;
+  return total;
 }
 
 module.exports = { calculateEntry, countEntrants };

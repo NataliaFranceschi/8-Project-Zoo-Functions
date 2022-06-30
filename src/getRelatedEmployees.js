317096b60722065f8e48dfc6d908b0d1f5567f94
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2022-06-30 14:02:42 -0300

    Requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,18 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function isManager(id) {
-  // seu código aqui
+  const getId = employees.map((element) => element.managers);
+  return getId.some((element) => element.find((ids) => ids === id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  const relatedEmployees = employees.filter((id) => id.managers.includes(managerId));
+  if (relatedEmployees.length === 0) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  return relatedEmployees.map((element) => `${element.firstName} ${element.lastName}`);
 }
 
 module.exports = { isManager, getRelatedEmployees };

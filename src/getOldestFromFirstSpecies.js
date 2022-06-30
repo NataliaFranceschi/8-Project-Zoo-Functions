471051d4577a8d4e13db288c43c222dac11bb590
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2022-06-30 14:47:39 -0300

    Requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
+const { species, employees } = data;
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const getAnimalId = employees.find((element) => element.id === id).responsibleFor[0];
+  const getAnimal = species.filter((animal) => animal.id === getAnimalId);
+  const getOldest = getAnimal[0].residents.reduce((acc, resident) => {
+    if (acc.age > resident.age) {
+      return acc;
+    } return resident;
+  });
+  const { age, name, sex } = getOldest;
+  return [name, sex, age];
 }
 
+console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 module.exports = getOldestFromFirstSpecies;

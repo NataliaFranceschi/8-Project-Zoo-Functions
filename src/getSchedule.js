9554ab09b71b2c019262a45f77d5b8ab9d3968e9
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2022-06-30 19:23:36 -0300

    Requisito 9

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,42 @@
 const data = require('../data/zoo_data');
 
+const { species, hours } = data;
+
+function getAnimal(element) {
+  const animals = species.filter((animal) => animal.availability.includes(element));
+  if (animals.length === 0) {
+    return 'The zoo will be closed!';
+  } return animals.map((animal) => animal.name);
+}
+
+function hour(element) {
+  if (hours[element].open === 0) {
+    return 'CLOSED';
+  } return `Open from ${hours[element].open}am until ${hours[element].close}pm`;
+}
+
+function schedule(param) {
+  const obj = {};
+  if (param === undefined) {
+    Object.keys(hours).forEach((element) => {
+      obj[[element]] = { officeHour: hour(element), exhibition: getAnimal(element) };
+    });
+  } else {
+    obj[[param]] = { officeHour: hour(param), exhibition: getAnimal(param) };
+  }
+  return obj;
+}
+console.log(species.find((animal) => animal.name === 'lions').availability);
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  const days = Object.keys(hours).find((day) => day === scheduleTarget);
+  const animals = species.find((animal) => animal.name === scheduleTarget);
+  if (scheduleTarget === undefined) {
+    return schedule();
+  } if (days !== undefined) {
+    return schedule(scheduleTarget);
+  } if (animals !== undefined) {
+    return species.find((animal) => animal.name === scheduleTarget).availability;
+  } return schedule();
 }
 
 module.exports = getSchedule;
